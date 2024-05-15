24194222f03c9168014d07cecd9ac4e243a762ec
Vitor Antonio Zidiotti de Souza <vitorzidiotti05@gmail.com>
2024-05-15 20:25:03 -0300

    feat: Iniciei o projeto e criei os forms e classes

---
+++ src/main/java/exercicio/projetobilheteria/Classes/MetodoPagamento.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio.projetobilheteria.Classes;
+
+/**
+ *
+ * @author vitor
+ */
+public class MetodoPagamento {
+    public int id;
+    
+    public String nome;
+    
+    
+}
---
+++ src/main/java/exercicio/projetobilheteria/ProjetoBilheteria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exercicio.projetobilheteria;
+
+/**
+ *
+ * @author vitor
+ */
+public class ProjetoBilheteria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        
+        var home = new FormHome();
+        
+        home.setVisible(true);
+    }
+}
